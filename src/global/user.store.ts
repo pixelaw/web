af455220cd651ec9356a29aa8080c619c881b785
Caspar Oostendorp <caspar@oostvoort.com>
2024-05-05 05:45:49 +0800

    fixed error with setter that was returning state

--- src/global/user.store.ts
+++ src/global/user.store.ts
@@ -3,45 +3,51 @@ import {immer} from "zustand/middleware/immer";
 import {NotificationDataType, PositionWithAddressAndType} from "./types";
 
 export interface IUserStore {
-  gameMode: string;
-  positionWithAddressAndType: PositionWithAddressAndType;
-  selectedHexColor: string;
-  zoomLevel: number;
-  notificationData: NotificationDataType | undefined;
-  setGameMode(data: string): void;
-  set(data: Partial<IUserStore>): void;
+    gameMode: string;
+    positionWithAddressAndType: PositionWithAddressAndType;
+    selectedHexColor: string;
+    zoomLevel: number;
+    notificationData: NotificationDataType | undefined;
+
+    setGameMode(data: string): void;
+
+    set(data: Partial<IUserStore>): void;
 }
 
 
 const useGameStore = create<IUserStore>()(
-  immer((set) => ({
-    gameMode: "paint",
-    positionWithAddressAndType: {
-      x: 0,
-      y: 0,
-      address: '',
-      pixel: '',
-    },
-    selectedHexColor: '#FFFFFF',
-    zoomLevel: 50,
-    notificationData: undefined,  
-    setGameMode: (data: string) => {
-      set(state => state.gameMode = data);
-    },
-    set: (data: Partial<IUserStore>) => {
-      set(state => {
-        Object.assign(state, data);
-      })
-    },
-  }))
+    immer((set) => ({
+        gameMode: "paint",
+        positionWithAddressAndType: {
+            x: 0,
+            y: 0,
+            address: '',
+            pixel: '',
+        },
+        selectedHexColor: '#FFFFFF',
+        zoomLevel: 50,
+        notificationData: undefined,
+        setGameMode: (data: string) => {
+            set(state => {
+                state.gameMode = data;
+                return;
+            });
+        },
+        set: (data: Partial<IUserStore>) => {
+            set(state => {
+                Object.assign(state, data);
+                return;
+            })
+        },
+    }))
 );
 
-const {getState : getGameStore} = useGameStore;
+const {getState: getGameStore} = useGameStore;
 export {getGameStore, useGameStore};
 
-/** 
+/**
  * Example usage
-*/ 
+ */
 
 // getGameStore().set({gameMode: "paint"})
 // getGameStore().gameMode;
