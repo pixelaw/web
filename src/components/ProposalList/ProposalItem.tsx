da0d691504976ba6aa0ffa7400d8ee9a5c031ba7
0xshora <112358132134.fibon@gmail.com>
2024-06-27 16:53:21 +0100

    fix: modified UI

--- src/components/ProposalList/ProposalItem.tsx
+++ src/components/ProposalList/ProposalItem.tsx
@@ -16,8 +16,8 @@ type PropsType = {
 
 const createProposalTitle = (proposalType: ProposalType, hexColor: string) => {
     switch (proposalType) {
-        case ProposalType.AddNewColor: return `Adding A New Color: ${hexColor}`
-        case ProposalType.MakeADisasterByColor: return `Make a Disaster by Color: ${hexColor}`
+        case ProposalType.AddNewColor: return `Adding A New Color: ${hexColor.toUpperCase()}`
+        case ProposalType.MakeADisasterByColor: return `Make a Disaster by Color: ${hexColor.toUpperCase()}`
         default: {
             console.error('unhandled proposal type: ', proposalType)
             return ''
@@ -102,6 +102,12 @@ const ProposalItem: React.FC<PropsType> = ({ entityId, onStartVote, filter, sear
             })
     }
 
+    const formatWalletAddress = (author: string) => {
+        if (author.length > 10) {
+            return `${author.slice(0, 4)}...${author.slice(-4)}`;
+        }
+        return author;
+    };
 
 
     return (
@@ -109,7 +115,7 @@ const ProposalItem: React.FC<PropsType> = ({ entityId, onStartVote, filter, sear
              className='relative bg-gray-800 p-4 rounded-md border border-gray-700 hover:border-gray-600 transition-colors duration-300'>
             <div className='block'>
                 <div className='flex justify-between items-center mb-1'>
-                    <div className='text-xl font-bold text-white flex items-center'>
+                    <div className='text-sm font-bold text-white flex items-center'>
                         {title}
                         {hexColor && (
                             <div
@@ -119,12 +125,12 @@ const ProposalItem: React.FC<PropsType> = ({ entityId, onStartVote, filter, sear
                         )}
                     </div>
                     <div
-                        className={`px-2 py-1 rounded-md text-white text-sm ${getStatusColor(proposalStatus)}`}>
+                        className={`px-2 py-1 rounded-md text-white text-xs ${getStatusColor(proposalStatus)}`}>
                         {proposalStatus}
                     </div>
                 </div>
-                <div className='text-gray-400 text-sm mb-2'>
-                    proposed by {proposal.author.toString()}
+                <div className='text-gray-400 text-xs mb-2'>
+                    proposed by {formatWalletAddress(proposal.author.toString())}
                 </div>
                 <div className='bg-gray-700 rounded-full h-2 relative flex mb-1 mr-20'>
                     <div
@@ -136,7 +142,7 @@ const ProposalItem: React.FC<PropsType> = ({ entityId, onStartVote, filter, sear
                         style={{width: `${(proposal.no_px / (proposal.yes_px + proposal.no_px)) * 100}%`}}
                     ></div>
                 </div>
-                <div className='flex justify-between text-sm text-gray-300 mr-20'>
+                <div className='flex justify-between text-xs text-gray-300 mr-20'>
                     <div>
                         For {proposal.yes_px} points
                     </div>
@@ -146,11 +152,13 @@ const ProposalItem: React.FC<PropsType> = ({ entityId, onStartVote, filter, sear
                 </div>
             </div>
             <button
-                className={`absolute bottom-4 right-4 px-4 py-2 rounded-md transition duration-300 bg-blue-600 hover:bg-blue-500 text-white`}
+                className={`absolute bottom-4 text-sm right-4 px-4 py-2 rounded-md transition duration-300 ${
+                    proposalStatus === 'closed' ? 'bg-gray-500 cursor-not-allowed' : 'bg-blue-600 hover:bg-blue-500 text-white'
+                }`}
                 onClick={() => proposalStatus === 'closed' ? handleActivateProposal() : onStartVote ? onStartVote(onStartVoteParam) : ''}
                 disabled={proposalStatus.includes('starts') || (!canActivateProposal && proposalStatus === 'closed')}
             >
-                {proposalStatus === 'closed' ? 'Start' : 'Vote'}
+                {proposalStatus === 'closed' ? 'Activate' : 'Vote'}
             </button>
         </div>
     )
