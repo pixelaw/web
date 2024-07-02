3930aeb4bcece8ab980ac140e2425470537a6bbf
mariz <marrise@oostvoort.com>
2024-07-02 21:28:28 +0800

    fix: color palette not dynamically changing

--- src/components/ColorPicker/SimpleColorPicker.tsx
+++ src/components/ColorPicker/SimpleColorPicker.tsx
@@ -1,11 +1,6 @@
 import styles from './SimpleColorPicker.module.css';
-import {useEntityQuery} from "@dojoengine/react";
-import {usePixelawProvider} from "@/providers/PixelawProvider.tsx";
-import {Has, HasValue, Entity} from "@dojoengine/recs";
-import PaletteColor from "@/components/ColorPicker/PaletteColor.tsx";
 import SimpleColorPickerItem from "@/components/ColorPicker/SimpleColorPickerItem.tsx";
-import {GAME_ID, PALETTE_COLORS_LENGTH} from "@/global/constants.ts";
-import {getEntityIdFromKeys} from "@dojoengine/utils";
+import usePaletteColors from "@/hooks/usePaletteColors.ts";
 
 const colors = [
     "#FF0000",
@@ -24,48 +19,23 @@ export interface ColorPickerProps {
     color: string;
 }
 
-const sortIndices = () => {
-    const sortedIndices: Entity[] = []
-    for (let i = 0; i < PALETTE_COLORS_LENGTH; i++) {
-        sortedIndices.push(
-            getEntityIdFromKeys([
-                BigInt(GAME_ID),
-                BigInt(i)
-            ])
-        )
-    }
-    return sortedIndices
-}
-
-const sortedIndices = sortIndices()
 
 const SimpleColorPicker: React.FC<ColorPickerProps> = ({onColorSelect, color: selectedColor}) => {
     selectedColor = `#${selectedColor}`
 
-    const {gameData} = usePixelawProvider();
+    const paletteColors = usePaletteColors()
 
-    const paletteColors = useEntityQuery(
-        [
-            Has(gameData!.setup.clientComponents.PaletteColors),
-            HasValue(gameData!.setup.clientComponents.PaletteColors, {game_id: GAME_ID}),
-        ], { updateOnValueChange: true }
-    )
-
-    const orderedPaletteColors = sortedIndices.filter(sortedIndex => {
-        const bigIntSortedIndex = BigInt(sortedIndex)
-        return paletteColors.findIndex(paletteColor => BigInt(paletteColor) === bigIntSortedIndex) > -1
-    })
-
-    if (paletteColors.length) {
+    if (paletteColors.data?.length) {
         return (
             <div className={styles.inner}>
-                {orderedPaletteColors.map(paletteColor => (
-                    <PaletteColor
-                        key={paletteColor.toString()}
-                        entityId={paletteColor}
+                {(paletteColors?.data ?? []).map(({color, idx}) => (
+                    <SimpleColorPickerItem
+                        key={color}
+                        color={color}
                         onSelect={onColorSelect}
                         selectedColor={selectedColor}
-                        lastIndex={paletteColors.length - 1}
+                        old={idx === 0}
+                        latest={idx === (paletteColors.data?.length ?? 0) - 1}
                     />
                 ))}
             </div>
