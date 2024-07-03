140705d5bf46c79ddbe3d6be39877b2da7ecdc3b
mariz <marrise@oostvoort.com>
2024-07-03 21:39:04 +0800

    feat: pop up new proposal created

--- src/hooks/useProposals.ts
+++ src/hooks/useProposals.ts
@@ -1,8 +1,13 @@
-import { useQuery } from '@tanstack/react-query'
+import {useQuery, useQueryClient} from '@tanstack/react-query'
 // @ts-ignore
 import { GraphQLClient } from 'graphql-request';
 import {useSettingsStore} from "@/stores/SettingsStore.ts";
 import GetProposals from "@/../graphql/GetProposals.graphql";
+import {createClient} from "graphql-ws";
+import {useEffect} from "react";
+import {toastSuccess} from "@/components/Toast";
+import {shortenHex} from "@dojoengine/utils";
+import {usePixelawProvider} from "@/providers/PixelawProvider.tsx";
 
 export type ProposalDataType = {
     game_id: number,
@@ -38,10 +43,89 @@ const useProposals = (gameId: number) => {
                 const result: Data = await gqlClient.request(GetProposals, { game_id: gameId })
                 return result.proposalModels.edges.map(edge => edge.node)
             },
-            enabled: !!gameId,
-            refetchInterval: 2_000
+            enabled: !!gameId
         }
     )
 }
 
 export default useProposals
+
+const SUBSCRIPTION = `
+subscription {
+  entityUpdated {
+    id
+    keys
+    models {
+      ... on Proposal {
+        game_id
+        index
+        author
+        proposal_type
+        target_color
+        start
+        end
+        yes_px
+        no_px
+        is_activated
+      }
+    }
+  }
+}
+`
+
+type SubscriptionMessageType = {
+    data: {
+        entityUpdated: {
+            id: string,
+            keys: string[],
+            models: (
+                ProposalDataType | Record<string, never>)[]
+        }
+    }
+}
+
+export const useProposalSubscription = () => {
+    const settings = useSettingsStore()
+    const baseUrl = settings?.config?.toriiUrl ?? ''
+    const client = createClient({
+        url: baseUrl.replace('http', 'ws') + '/graphql'
+    })
+
+    const queryClient = useQueryClient();
+
+    const { gameData} = usePixelawProvider();
+
+    useEffect(() => {
+        const unsubscribe = client.subscribe(
+            { query: SUBSCRIPTION },
+            {
+                next: ({data}: SubscriptionMessageType) => {
+                    const proposalEntities = data
+                        .entityUpdated.models
+                        .filter(model => !!Object.keys(model).length) as unknown as ProposalDataType[]
+                    if (!proposalEntities.length) return
+                    queryClient.invalidateQueries({ queryKey: ['proposals']}).then()
+
+                    const newProposals = proposalEntities.filter(proposalEntity => {
+                        return proposalEntity.yes_px === 0 && proposalEntity.no_px === 0 && !proposalEntity.is_activated
+                    })
+
+
+                    newProposals.forEach(newProposal => {
+                        const isPlayer = gameData?.account?.account?.address?.toLowerCase() === newProposal.author.toLowerCase()
+                        const playerAddress = isPlayer ? 'You' : shortenHex(newProposal.author, 10)
+
+                        toastSuccess({
+                            message: `${playerAddress} just created a new proposal`
+                        })
+                    })
+
+                },
+                error: (err) => console.error(err),
+                complete: () => console.log('Subscription completed')
+            }
+        )
+
+        return () => unsubscribe()
+    }, [gameData, queryClient, client])
+}
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,18 @@ import '@/index.css'
 import {BrowserRouter} from 'react-router-dom';
 import {PixelawProvider} from "@/providers/PixelawProvider.tsx";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {useProposalSubscription} from "@/hooks/useProposals.ts";
 
 const queryClient = new QueryClient()
 
 
 console.log(import.meta.env)
+
+// putting it here for now because App updates like Crazy
+const Test = () => {
+    useProposalSubscription()
+    return <></>
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
@@ -17,6 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             <PixelawProvider>
                 <BrowserRouter>
                     <App/>
+                    <Test />
                 </BrowserRouter>
             </PixelawProvider>
         </QueryClientProvider>
