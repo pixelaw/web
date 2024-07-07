04f1f035311f4eaa5f5b480cbd0cec7701f28456
Caspar Oostendorp <thiscaspar@gmail.com>
2024-07-07 14:40:57 +0200

    Refactor utils.ts for better readability and type imports

--- src/global/utils.ts
+++ src/global/utils.ts
@@ -1,49 +1,49 @@
-import {shortString} from 'starknet'
-import {Coordinate} from "@/webtools/types.ts";
-import {Position} from "@/global/types.ts";
+import type { Position } from "@/global/types.ts"
+import type { Coordinate } from "@/webtools/types.ts"
+import { shortString } from "starknet"
 
 /*
-* @notice converts a number to hexadecimal
-* @param n is the decimal number to convert to hexadecimal
-* */
+ * @notice converts a number to hexadecimal
+ * @param n is the decimal number to convert to hexadecimal
+ * */
 export const convertToHexadecimal = (n: number) => n.toString(16)
 export const prefixString = (prefix: string, base: string) => `${prefix}${base}`
-export const convertToHexadecimalAndLeadWithOx = (n: number) => prefixString('0x', convertToHexadecimal(n))
+export const convertToHexadecimalAndLeadWithOx = (n: number) =>
+    prefixString("0x", convertToHexadecimal(n))
 export const convertToDecimal = (hexadecimalString: string) => {
     const n = hexadecimalString.replace("0x", "")
-    return parseInt(n, 16);
+    return Number.parseInt(n, 16)
 }
 
 // Function to convert a ReadableStream to a string
 export async function streamToString(readableStream: ReadableStream) {
-    const textDecoder = new TextDecoder();
-    const reader = readableStream.getReader();
-    let result = '';
+    const textDecoder = new TextDecoder()
+    const reader = readableStream.getReader()
+    let result = ""
 
     try {
-         
         while (true) {
-            const {done, value} = await reader.read();
+            const { done, value } = await reader.read()
 
             if (done) {
-                break; // The stream has ended
+                break // The stream has ended
             }
 
-            result += textDecoder.decode(value);
+            result += textDecoder.decode(value)
         }
 
-        return result;
+        return result
     } finally {
-        reader.releaseLock();
+        reader.releaseLock()
     }
 }
 
 export const felt252ToString = (felt252: string | number | bigint) => {
-    if (typeof felt252 === 'bigint' || typeof felt252 === 'object') {
+    if (typeof felt252 === "bigint" || typeof felt252 === "object") {
         felt252 = `0x${felt252.toString(16)}`
     }
-    if (felt252 === '0x0' || felt252 === '0') return ''
-    if (typeof felt252 === 'string') {
+    if (felt252 === "0x0" || felt252 === "0") return ""
+    if (typeof felt252 === "string") {
         try {
             return shortString.decodeShortString(felt252)
         } catch (e) {
@@ -55,9 +55,9 @@ export const felt252ToString = (felt252: string | number | bigint) => {
 
 export const felt252ToUnicode = (felt252: string | number) => {
     const string = felt252ToString(felt252)
-    if (string.includes('U+')) {
-        const text = string.replace('U+', '')
-        const codePoint = parseInt(text, 16)
+    if (string.includes("U+")) {
+        const text = string.replace("U+", "")
+        const codePoint = Number.parseInt(text, 16)
         return String.fromCodePoint(codePoint)
     }
     return string
@@ -65,7 +65,7 @@ export const felt252ToUnicode = (felt252: string | number) => {
 
 export const formatAddress = (address: string) => {
     if (address.length > 30) {
-        return address.substr(0, 6) + '...' + address.substr(address.length - 4, address.length)
+        return `${address.substr(0, 6)}...${address.substr(address.length - 4, address.length)}`
     }
 
     return address
@@ -73,13 +73,12 @@ export const formatAddress = (address: string) => {
 
 // Takes a RGB hex nr and converts it to numeric rgba (0 alpha)
 export const coordinateToPosition = (coord: Coordinate): Position => {
-    return {x: coord[0], y: coord[1]}
+    return { x: coord[0], y: coord[1] }
 }
 
 export const hexRGBtoNumber = (color: string) => {
-    return parseInt(`0x${color}00`, 16)
+    return Number.parseInt(`0x${color}FF`, 16)
 }
-
 
 // Converts the numeric RGBA to a normal hex color
 // @dev this removes the Alpha channel.
@@ -87,16 +86,15 @@ export const hexRGBtoNumber = (color: string) => {
 // ref: https://developer.mozilla.org/en-US/docs/Web/CSS/color_value
 export const numRGBAToHex = (rgba: number) => {
     const color = rgba >>> 8
-    return '#' + (color).toString(16).padStart(6, "0")
+    return `#${color.toString(16).padStart(6, "0")}`
 }
 
-
 export const removeNullsFromArray = <T>(array: (T | null)[]) => {
-    return array.filter(element => element !== null) as T[]
+    return array.filter((element) => element !== null) as T[]
 }
 export const formatWalletAddress = (address: string) => {
     if (address.length > 10) {
-        return `${address.slice(0, 4)}...${address.slice(-4)}`;
+        return `${address.slice(0, 4)}...${address.slice(-4)}`
     }
-    return address;
-};
+    return address
+}
